8b05681d7b88713e00696aa0d0d3a270241eb2c7
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-24 22:55:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,11 @@ katz_deli = []
 
 def line 
   if katz_deli.length == 0 
-    puts ""
+    puts "The line is currently empty."
+  else
+    puts katz_deli
+end
+
+def take_a_number (, person_in_line)
+
 end
